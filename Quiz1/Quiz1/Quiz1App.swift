7dd3a29403184cec1d9da6b73ec04c87d4628cec
HienLuu07 <luudinhhien9999@gmail.com>
2026-09-14 15:14:42 +0700

    Add Quiz 1

---
+++ Quiz1/Quiz1/Quiz1App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz1App.swift
+//  Quiz1
+//
+//  Created by Macbook on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Quiz1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
